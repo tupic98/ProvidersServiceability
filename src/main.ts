38f9e4109eb2f6a1cf1783822fc3fd7c78af7b2b
tupic98 <00017316@uca.edu.sv>
2019-09-12 11:11:41 -0600

    Enabling swagger documentation and enabling cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const options = new DocumentBuilder()
+    .setTitle('Providers Serviceability by ZIP API')
+    .setDescription('Serviceability information across US country for providers')
+    .setVersion('1.0.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('documentation', app, document);
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
